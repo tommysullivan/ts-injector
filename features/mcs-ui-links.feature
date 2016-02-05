461412614a163e36e92f62be89636995c6de16fe
Tommy Sullivan <tsullivan@maprtech.com>
2016-02-05 01:51:09 -0800

    added MCS url link checking
    it is broken for me but that is because the nodenames are not reporting
    correctly as set up by mitra during env provisioning and thus dns query fails
    however the rest looks good.
    
    #SPYG-126

---
+++ features/mcs-ui-links.feature
@@ -0,0 +1,15 @@
+@SPYG-126
+Feature: MCS UI Links
+
+  Scenario Outline:
+    Given my MCS username is "mapr"
+    And my MCS password is "mapr"
+    And my MCS is running at "https://10.10.1.102:8443"
+    And I have an authenticated MCS Rest Client Session
+    When I ask for a link to <application>
+    Then I receive a URL to <application>
+    And a GET request of the URL does not return an error status code
+    Examples:
+      | application |
+      | grafana     |
+      | kibana      |
---
+++ features/step_definitions/mcs-ui-link-steps.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+    this.When(/^I ask for a link to (.*)$/, function (applicationName, callback) {
+        var self = this;
+        this.authenticatedMCSSession.applicationLinkFor(applicationName).done(
+            function(applicationURLFromMCS) {
+                self.applicationURLFromMCS = applicationURLFromMCS;
+                callback();
+            },
+            callback
+        )
+    });
+
+    this.Then(/^I receive a URL to (.*)$/, function (applicationName, callback) {
+        if(!this.api.newURLValidator().isUri(this.applicationURLFromMCS)) callback("Invalid URL: "+this.applicationURLFromMCS);
+        else callback();
+    });
+
+    this.Given(/^a GET request of the URL does not return an error status code$/, function () {
+        return this.api.newRestClientAsPromised().get(this.applicationURLFromMCS);
+    });
+}
