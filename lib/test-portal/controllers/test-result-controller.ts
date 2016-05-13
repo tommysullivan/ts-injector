f25311293868698d9c6693fa83cacc57ee0eade8
Tommy Sullivan <tsullivan@maprtech.com>
2016-05-13 13:46:50 -0700

    speeding up index by not sending unused json data
    JIRA: #DEVOPS-410

--- lib/test-portal/controllers/test-result-controller.ts
+++ lib/test-portal/controllers/test-result-controller.ts
@@ -9,6 +9,7 @@ export default class TestResultController extends BaseExpressController {
     private fs:IFileSystem;
     private filePathHelper:FilePathHelper;
     private errorHandler:ErrorHandler;
+    private contentTypeThatTransformsToDesiredContentType:string = 'vnd/mapr.test-portal.cluster-test-result+json;v=2.0.0';
 
     constructor(fs:IFileSystem, filePathHelper:FilePathHelper, errorHandler:ErrorHandler) {
         super();
@@ -20,11 +21,44 @@ export default class TestResultController extends BaseExpressController {
     get(httpRequest:IHttpRequest, httpResponse:IHttpResponse):void {
         if(httpRequest.accepts('text/html'))
             httpResponse.redirect(`/#/test-result-viewer/${httpRequest.params.getOrThrow('resultId')}`);
-        else
-            this.fs.createReadStream(
-                this.filePathHelper.getTestResultFilePathFromHttpRequest(httpRequest)
-            ).pipe(httpResponse);
+        else {
+            var filePath = this.filePathHelper.getTestResultFilePathFromHttpRequest(httpRequest);
+            if(this.shouldAttemptTransform(httpRequest, filePath)) {
+                this.getTestResultSummary(httpRequest, httpResponse, filePath);
+            } else {
+                this.fs.createReadStream(
+                    this.filePathHelper.getTestResultFilePathFromHttpRequest(httpRequest)
+                ).pipe(httpResponse);
+            }
+        }
     }
+
+    private shouldAttemptTransform(httpRequest:IHttpRequest, filePath:string):boolean {
+        if(httpRequest.accepts('vnd/mapr.test-portal.cluster-test-result-summary+json;v=1.0.0')) {
+            var testResultJSON = this.fs.readJSONObjectFileSync(filePath);
+            var hasContentType = testResultJSON.hasPropertyNamed('contentType');
+            var canTransformToContentType = testResultJSON.stringPropertyNamed('contentType') == this.contentTypeThatTransformsToDesiredContentType;
+            return canTransformToContentType;
+        }
+        else return false;
+    }
+
+    private getTestResultSummary(httpRequest:IHttpRequest, httpResponse:IHttpResponse, filePath:string):void {
+        var testResultJSON = this.fs.readJSONObjectFileSync(filePath);
+        var transformedTestResultJSON = {
+            contentType: testResultJSON.stringPropertyNamed('contentType'),
+            clusterConfiguration: {
+                id: testResultJSON.jsonObjectNamed('clusterConfiguration').stringPropertyNamed('id')
+            },
+            cucumberTestResult: {
+                cucumberFeatureResults: testResultJSON.hasPropertyNamed('cucumberTestResult')
+                    ? testResultJSON.jsonObjectNamed('cucumberTestResult').listNamed('cucumberFeatureResults').toArray()
+                    : []
+            }
+        }
+        httpResponse.end(JSON.stringify(transformedTestResultJSON));
+    }
+
 
     put(httpRequest:IHttpRequest, httpResponse:IHttpResponse):void {
         try {
