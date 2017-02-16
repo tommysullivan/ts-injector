bfa921ed35d8f3f42e1ca48fd1598e68446429c9
Aashreya Ravi Shankar <ashankar@mapr.com>
2017-02-16 13:54:47 -0800

    Adding ubuntu image for run. (#74)

--- bin/install-mapr-test-cluster.sh
+++ bin/install-mapr-test-cluster.sh
@@ -3,7 +3,7 @@ set -e
 bin/prepare-dependencies.sh
 export release=mapr6.0.0
 export lifecyclePhase=development
-export onDemandClusters=MesosDockerFarm:baseCentOS1Node
+export onDemandClusters=MesosDockerFarm:baseCentOS1Node,MesosDockerFarm:baseUbuntu141Node
 export portalId=lab
 npm run build
 bin/devops run cucumber -- --tags @packageInstallation
