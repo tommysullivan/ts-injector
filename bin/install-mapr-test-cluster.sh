a6478e95d3576baf5729730577f43bf86323876e
Aashreya Ravi Shankar <ashankar@mapr.com>
2017-03-03 10:45:26 -0800

    Adding 5.2.1 promotion check #DEVOPS-1227 (#85)
    * Adding 5.2.1 promotion check #DEVOPS-1227
    
    Version update
    
    * Updating test name

--- bin/install-mapr-test-cluster.sh
+++ bin/install-mapr-test-cluster.sh
@@ -5,5 +5,9 @@ export release=mapr6.0.0
 export lifecyclePhase=development
 export onDemandClusters=MesosDockerFarm:baseCentOS1Node,MesosDockerFarm:baseUbuntu141Node
 export portalId=lab
+export testName=core-6.0-promotion
 npm run build
+bin/devops run featureSet --featureSetId dockerInstallAndPackageCheck
+export release=mapr5.2.1
+export testName=core-5.2.1-promotion
 bin/devops run featureSet --featureSetId dockerInstallAndPackageCheck
