b54b22890da2667e9fce126f423b95dd389aca81
Anagha Naik <Angha@users.noreply.github.com>
2017-03-08 13:16:28 -0800

    Fixing i-open-tsdb-rest-client.ts for Spyglass (#90)

--- lib/open-tsdb/i-open-tsdb-rest-client.ts
+++ lib/open-tsdb/i-open-tsdb-rest-client.ts
@@ -1,3 +1,8 @@
+import {IFuture} from "../futures/i-future";
+import {IDictionary} from "../collections/i-dictionary";
+import {IOpenTSDBResult} from "./i-open-tsdb-result";
 export interface IOpenTSDBRestClient {
+    queryForMetric(startTime:string, metricName:string):IFuture<IOpenTSDBResult>;
+    queryForMetricWithTags(startTime:string, metricName:string, soughtTags:IDictionary<string>):IFuture<IOpenTSDBResult>;
 
 }
